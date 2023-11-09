2e010bb9ade1c9b2a728c96c7f6ab1ff8f064963
mshandil <146214158+mshandil@users.noreply.github.com>
2023-11-09 04:27:42 +0000

    final

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,60 @@
+class ZebraController < ApplicationController
+  def hp
+    render template: "dice_template/hp"
+  end
+
+  def two_six
+    @rolls = []
+
+    2.times do
+      die = rand(1..6)
+      @rolls.push(die)
+    end
+
+    render template: "dice_template/two_six"
+  end
+
+  def two_ten
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+      @rolls.push(die)
+    end
+
+    render template: "dice_template/two_ten"
+  end
+
+  def one_twenty
+    @rolls = []
+
+    1.times do
+      die = rand(1..20)
+      @rolls.push(die)
+    end
+
+    render template: "dice_template/one_twenty"
+  end
+
+  def five_four
+    @rolls = []
+
+    5.times do
+      die = rand(1..4)
+      @rolls.push(die)
+    end
+
+    render template: "dice_template/five_four"
+  end
+
+  def random
+    @rolls = []
+   
+    @num1.times do
+      die = rand(1..@num2)
+      @rolls.push(die)
+    end
+
+    render template: "dice_template/random"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "zebra", :action => "hp" })
+  get("/dice/2/6", { :controller => "zebra", :action => "two_six" })
+  get("/dice/2/10", { :controller => "zebra", :action => "two_ten" })
+  get("/dice/1/20", { :controller => "zebra", :action => "one_twenty" })
+  get("/dice/5/4", { :controller => "zebra", :action => "five_four" })
+  get("/dice/:@num1/:@num2", { :controller => "zebra", :action => "random" })
 end
